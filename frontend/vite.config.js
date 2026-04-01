4fb5634242b5bfd04980d9035ea2c18411a5fc30
Arjun-JUN <109136876+Arjun-JUN@users.noreply.github.com>
2026-04-01 21:54:55 +0530

    Migrate to vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'REACT_APP_');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: 3000,
+    },
+    build: {
+      outDir: 'build',
+    },
+    define: {
+      'process.env': {
+        ...env,
+        NODE_ENV: JSON.stringify(mode),
+      },
+    },
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.js$/,
+      exclude: [],
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx',
+        },
+      },
+    },
+  };
+});
